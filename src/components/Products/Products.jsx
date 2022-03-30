6c89045204a7835d19f992179e178ce5e32d04ce
Sahil Kedare <sahilkedare4444@gmail.com>
2022-03-30 23:13:59 +0530

    added product card

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import Product from "./Product/Product";
+
+const products = [
+  { id: 1, name: "Shoes", description: "Running Shoes", price: "$10", image:"https://media.istockphoto.com/photos/modern-sport-shoes-picture-id623270836?k=20&m=623270836&s=612x612&w=0&h=C0WdoMeoHYugJy8mVgrTl8p1U8DltiZ25AzzjGY05GA=" },
+  { id: 2, name: "Acer", description: "Running Laptop", price: "$5", image:"https://static.acer.com/up/Resource/Acer/Laptops/Swift_3/Image/20200416/Acer-Swift-3_SF314-42_Silver-FP-Backlit_modelpreview.png" },
+];
+
+const Products = () => {
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
