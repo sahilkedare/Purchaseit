bc7b28cf3e7979895a2160d84c3b7eb7239b7afc
Sahil Kedare <sahilkedare4444@gmail.com>
2022-03-31 10:12:03 +0530

    navbar added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { AppBar, Toolbar, IconButton, Badge,Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+// import { Link, useLocation } from 'react-router-dom';
+
+import useStyles from './styles';
+import logo from '../../assets/commerce.png'
+
+const Navbar = () => {
+    const classes = useStyles()
+  return (
+      <>
+          <AppBar position="fixed" className={classes.appBar} color="inherit">
+              <Toolbar>
+                  <Typography variant="h6" className={classes.title} color="inherit">
+                      <img src={logo} alt="commerce.js" height="25px" className={classes.image} />
+                      Commerce.js
+                  </Typography>
+                  <div className={classes.grow} />
+                  <div className={classes.button}>
+                      <IconButton aria-label="Show cart items" color="inherit">
+                          <Badge badgeContent={2} color="secondary">
+                              <ShoppingCart/>
+                          </Badge>
+                      </IconButton>
+                  </div>
+              </Toolbar>
+          </AppBar> 
+      </>
+  )
+}
+
+export default Navbar
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 
 import Product from "./Product/Product";
+import useStyles from './styles'
 
 const products = [
   { id: 1, name: "Shoes", description: "Running Shoes", price: "$10", image:"https://media.istockphoto.com/photos/modern-sport-shoes-picture-id623270836?k=20&m=623270836&s=612x612&w=0&h=C0WdoMeoHYugJy8mVgrTl8p1U8DltiZ25AzzjGY05GA=" },
@@ -9,8 +10,10 @@ const products = [
 ];
 
 const Products = () => {
+  const classes = useStyles();
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar}/>
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
