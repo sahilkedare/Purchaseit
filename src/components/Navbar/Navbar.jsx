164477d0d2409c77265a21d3fc7d177bbfbbd2aa
Sahil Kedare <sahilkedare4444@gmail.com>
2022-03-31 16:29:06 +0530

    initialised commercejs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
               <Toolbar>
                   <Typography variant="h6" className={classes.title} color="inherit">
                       <img src={logo} alt="commerce.js" height="25px" className={classes.image} />
-                      Commerce.js
+                      Shopify
                   </Typography>
                   <div className={classes.grow} />
                   <div className={classes.button}>
