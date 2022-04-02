8335f0e1a565b054ee8a7598e9b1fbd658eb3a51
Sahil Kedare <sahilkedare4444@gmail.com>
2022-04-02 21:16:27 +0530

    address form done

--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -3,23 +3,14 @@ import { TextField, Grid } from '@material-ui/core';
 import { useFormContext, Controller } from 'react-hook-form';
 
 
-const FormInput = ({ name, label, required }) => {
+const FormInput = ({ name, label }) => {
   const { control } = useFormContext();
   return (
     <Grid item xs={12} sm={6}>
-      <Controller
-        defaultValue=""
-        control={control}
-        name={name}
+      <Controller  defaultValue=""  control={control}  name={name}
         render={({ field }) => (
-          <TextField
-            {...field}
-            name={name}
-            label={label}
-            required={required}
-            fullWidth
-          />
-        )}
+          <TextField  {...field}  name={name}  label={label}  required  fullWidth
+          /> )}
       />
     </Grid>
   );
